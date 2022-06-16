4799f701651e93a22a20e0bc54a0e9c15cc60e93
TKC-03 <107327394+TKC-03@users.noreply.github.com>
2022-06-16 03:18:19 -0500

    Update filter_gas.py

--- filter_gas.py
+++ filter_gas.py
@@ -3,7 +3,7 @@ from web3 import Web3
 
 
 w3 = Web3(Web3.HTTPProvider(
-    'https://ropsten.infura.io/v3/073da3011a37435a926c6adecdf0e9e5'))
+    'https://ropsten.infura.io/v3/insert_your_endpoint'))
 a = w3.isConnected()
 
 print(a)
